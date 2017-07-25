e726bdc930e2261d07d9e0a4b48dd09f2c0792fa
sivadass <nsivadass@gmail.com>
2017-07-25 12:54:37 +0530

    Added API Key

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ class App extends React.Component{
 
       // 2. Initialize the JavaScript client library.
       gapi.client.init({
-        'apiKey': '992549188018',
+        'apiKey': 'AIzaSyAAetBokn47cnOcfyoSiNX8rOpeI87ZdaU',
         // Your API key will be automatically added to the Discovery Document URLs.
         'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
         // clientId and scope are optional if auth is not required.
-        'clientId': '3prg54pp18je3e3qhgcttgl11491c4dm.apps.googleusercontent.com',
+        'clientId': '992549188018-3prg54pp18je3e3qhgcttgl11491c4dm.apps.googleusercontent.com',
         'scope': 'https://www.googleapis.com/auth/calendar',
       }).then(function() {
         // 3. Initialize and make the API request.
@@ -22,9 +22,10 @@ class App extends React.Component{
         //   'resourceName': 'people/me',
         //   'requestMask.includeField': 'person.names'
         // });
-        console.log('hi')
+        console.log('hi');
+        gapi.auth2.getAuthInstance();
       }).then(function(response) {
-        console.log(response.result);
+        console.log(response);
       }, function(reason) {
        // console.log('Error: ' + reason.result.error.message);
       });
